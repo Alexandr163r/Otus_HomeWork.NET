5552a10af11c6ff4c551129e4bc12ffb20262e28
ulyanovav <ulyanovav@intellect-soft.ru>
2025-05-08 15:08:39 +0400

    добавлена проверка на пустую строку

--- HomeWork/Homework_3/OtusTask/Program.cs
+++ HomeWork/Homework_3/OtusTask/Program.cs
@@ -3,9 +3,14 @@ using System.Diagnostics;
 using OtusTask;
 
 var stopwatch = Stopwatch.StartNew();
-var derictoryFiles = Console.ReadLine();
+var directoryFiles = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(directoryFiles))
+{
+    Console.WriteLine("Путь к директории не может быть пустым!");
+    return;
+}
 
 var txtSpaceCounter = new TxtSpaceCounter();
-await txtSpaceCounter.CountSpacesInDirectoryAsynс(derictoryFiles);
-
+await txtSpaceCounter.CountSpacesInDirectoryAsynс(directoryFiles);
 Console.WriteLine($"Время выполнения {stopwatch.Elapsed}");
